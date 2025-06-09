bed3e80053e44f469c2c892e9335b8dfbb5e0010
yunus <yunus.polat@simdatix.com>
2025-06-09 21:41:36 +0200

    delete useEffect on useAutoSolver.ts and refactor it

--- src/hooks/useAutoSolver.ts
+++ src/hooks/useAutoSolver.ts
@@ -1,11 +1,13 @@
 import { sudokuBlocks } from "@/data/sudokuData";
+import { useRef, useCallback, useEffect } from "react";
 import { UseSudokuSolverParams } from "@/types/sudokuTypes";
 import {
   findBlock,
-  getValuesFromSquares,
-  updateSquareValue,
-} from "@/utils/sudoku";
-import { useRef, useCallback, useEffect, useState } from "react";
+  getEmptySquares,
+  focusSquareById,
+  findValidNumber,
+  getValuesToCompare,
+} from "@/utils/sudokuHelpers";
 
 export const useSudokuSolver = ({
   squaresInRow,
@@ -17,10 +19,6 @@ export const useSudokuSolver = ({
   setSudokuValues,
   setSquaresInSelectedBlock,
 }: UseSudokuSolverParams) => {
-  //! states
-  const [selectedRow, setSelectedRow] = useState<number | null>(null);
-  const [selectedCol, setSelectedCol] = useState<number | null>(null);
-
   //! hooks
   const sudokuValuesRef = useRef(sudokuValues);
 
@@ -28,70 +26,20 @@ export const useSudokuSolver = ({
     sudokuValuesRef.current = sudokuValues;
   }, [sudokuValues]);
 
-  useEffect(() => {
-    if (!squaresInRow || !squaresInCol || !squaresInSelectedBlock) return;
-
-    const rowValues = getValuesFromSquares(squaresInRow, sudokuValues);
-    const colValues = getValuesFromSquares(squaresInCol, sudokuValues);
-    const blockValues = getValuesFromSquares(
-      squaresInSelectedBlock,
-      sudokuValues
-    );
-
-    //* Combine all values from row, column, and block
-    const allValues = [...rowValues, ...colValues, ...blockValues];
-    const uniqueValues = Array.from(new Set(allValues));
-
-    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const impossibleNumbers = uniqueValues.filter((item) => item !== 0);
-    const possibleNumbers = numbers.filter(
-      (item) => !impossibleNumbers.includes(item)
-    );
-
-    //* If exactly one possible value is left for this square, update it
-    if (possibleNumbers.length === 1) {
-      updateSquareValue(
-        `${selectedRow}-${selectedCol}`,
-        possibleNumbers[0],
-        setSudokuValues
-      );
-    }
-  }, [
-    squaresInRow,
-    squaresInCol,
-    squaresInSelectedBlock,
-    sudokuValues,
-    selectedCol,
-    selectedRow,
-    setSudokuValues,
-  ]);
-
   //! functions
-  const startFocusing = useCallback(async () => {
+  const startAutoSolve = useCallback(async () => {
     while (true) {
       const currentSudoku = sudokuValuesRef.current;
+      const emptySquareIDs = getEmptySquares(currentSudoku);
 
-      const zeroCells: string[] = [];
-
-      currentSudoku.forEach((row, rowIndex) => {
-        row.forEach((value, colIndex) => {
-          if (value === 0) {
-            zeroCells.push(`${rowIndex}-${colIndex}`);
-          }
-        });
-      });
-
-      if (zeroCells.length === 0) {
-        alert("İşlem bitti!");
+      //* Stop the startAutoSolve function when there are no empty squares left
+      if (emptySquareIDs.length === 0) {
+        alert("Sudoku wurde erfolgreich gelöst!");
         break;
       }
-
-      for (const id of zeroCells) {
-        const el = document.getElementById(id) as HTMLInputElement | null;
-        if (!el) continue;
-
-        el.focus();
-        await new Promise((resolve) => setTimeout(resolve, 500));
+      //* Focus each empty square one by one and trigger handleFocus for every empty square
+      for (const id of emptySquareIDs) {
+        await focusSquareById(id);
       }
     }
   }, []);
@@ -99,20 +47,29 @@ export const useSudokuSolver = ({
   const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
     const selectedSquareID = e.target.id;
     const [row, col] = selectedSquareID.split("-").map(Number);
-    setSelectedRow(row);
-    setSelectedCol(col);
-    const selectedBlock = findBlock(selectedSquareID);
+
     const rowSquares = Array.from({ length: 9 }, (_, i) => `${row}-${i}`);
     const colSquares = Array.from({ length: 9 }, (_, i) => `${i}-${col}`);
     setSquaresInRow(rowSquares);
     setSquaresInCol(colSquares);
+
+    const selectedBlock = findBlock(selectedSquareID);
     if (selectedBlock) {
       setSquaresInSelectedBlock(sudokuBlocks[selectedBlock]);
-      console.log(sudokuBlocks[selectedBlock]);
     } else {
       setSquaresInSelectedBlock(null);
     }
+
+    //* Find and apply valid number
+    const valuesToCompare = getValuesToCompare(
+      squaresInRow,
+      squaresInCol,
+      squaresInSelectedBlock,
+      sudokuValues
+    );
+
+    findValidNumber(valuesToCompare, setSudokuValues, selectedSquareID);
   };
 
-  return { startFocusing, handleFocus };
+  return { startAutoSolve, handleFocus };
 };
