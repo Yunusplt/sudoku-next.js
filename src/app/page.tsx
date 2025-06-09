7bb75a633d1e6dc222d1e8367e4666538c514d07
yunus <yunus.polat@simdatix.com>
2025-06-09 09:45:08 +0200

    add Sudoku component on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,45 @@
+"use client";
+import { useState } from "react";
+import { Button } from "@mui/material";
+import { example_1 } from "@/data/sudokuData";
+import { useSudokuSolver } from "@/hooks/useAutoSolver";
+import Sudoku from "@/components/Sudoku";
+
 export default function Home() {
-  const emptySudoku = Array.from({ length: 9 }, () => Array(9).fill(0));
+  //! states
+  const [sudokuValues, setSudokuValues] = useState<number[][]>(example_1);
+  const [squaresInRow, setSquaresInRow] = useState<string[] | null>(null);
+  const [squaresInCol, setSquaresInCol] = useState<string[] | null>(null);
+  const [squaresInSelectedBlock, setSquaresInSelectedBlock] = useState<
+    string[] | null
+  >(null);
 
-  console.log(emptySudoku);
+  //! custom hook useSudokuSolver
+  const { startFocusing, handleFocus } = useSudokuSolver({
+    sudokuValues,
+    squaresInRow,
+    squaresInCol,
+    squaresInSelectedBlock,
+    setSquaresInRow,
+    setSquaresInCol,
+    setSudokuValues,
+    setSquaresInSelectedBlock,
+  });
 
-  return <main>SUDOKU</main>;
+  return (
+    <main>
+      <Sudoku
+        sudokuValues={sudokuValues}
+        squaresInRow={squaresInRow}
+        squaresInCol={squaresInCol}
+        squaresInSelectedBlock={squaresInSelectedBlock}
+        setSudokuValues={setSudokuValues}
+        handleFocus={handleFocus}
+      />
+
+      <Button variant="contained" onClick={startFocusing}>
+        Löse
+      </Button>
+    </main>
+  );
 }
